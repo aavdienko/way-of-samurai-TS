5689d13b05c662cb267f4412ccafa1b225ab3712
aavdienko <asiavostok@seznam.cz>
2022-12-27 17:08:43 +0100

    Lesson 7. What is component?

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,32 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
+      <Header/>
+      <Technologies/>
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <div>
       <ul>
         <li>CSS</li>
         <li>HTML</li>
         <li>REACT</li>
+      </ul>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <a href='#s'>Home</a>
+        <a href='#s'>News Feed</a>
+        <a href='#s'>Messages</a>
       </ul>
     </div>
   );
