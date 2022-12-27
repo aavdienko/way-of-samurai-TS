2c08befa21f22ec41b1bbc9374a6411701b750f7
aavdienko <asiavostok@seznam.cz>
2022-12-27 17:51:24 +0100

    Lesson 9. Import-Export. Creating separate components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Technologies/>
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>CSS</li>
-        <li>HTML</li>
-        <li>REACT</li>
-      </ul>
-    </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <ul>
-        <a href='#s'>Home</a>
-        <a href='#s'>News Feed</a>
-        <a href='#s'>Messages</a>
-      </ul>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <a href='#s'>Home</a>
+        <a href='#s'>News Feed</a>
+        <a href='#s'>Messages</a>
+      </ul>
+    </div>
+  );
+}
+
+export default Header
