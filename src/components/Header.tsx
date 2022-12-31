2c0799f364c62477192718fa897b283003863820
aavdienko <asiavostok@seznam.cz>
2022-12-31 18:05:22 +0100

    Leeson 12. Created separate components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+      <header className='header'>
+        <img src='https://www.edigitalagency.com.au/wp-content/uploads/Twitter-logo-png.png'/>
+      </header>
+  )
+}
+
+export default Header
