cbde722d468926623bfba1e9ea5598fb35a82d04
aavdienko <asiavostok@seznam.cz>
2023-01-07 13:58:32 +0100

    Lesson 16. First props.

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import styles from './MyPosts.module.css'
+import styles from './MyPosts.module.css';
 import Post from './Post/Post';
-
-
-
 
 const MyPosts = () => {
   return (
-  <div>
-    <div>My Posts</div>
-    <textarea></textarea>
-    <button>Add Post</button>
-    <div className={styles.posts}>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
+    <div>
+      <div>My Posts</div>
+      <textarea></textarea>
+      <button>Add Post</button>
+      <div className={styles.posts}>
+        <Post message="Hi how are you?" likesCount='20'/>
+        <Post message="It is my first post" likesCount='30'/>
+      </div>
     </div>
+  );
+};
 
-</div>
-)}
-
-export default MyPosts
+export default MyPosts;
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import styles from './Post.module.css'
 
 
+type PostPropsType = {
+  message: string
+  likesCount: string
+}
 
-const Post = () => {
+const Post = (props: PostPropsType) => {
   return (
     <div className={styles.item}>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPhD7clYdj1yIC0yM_vJmxwniuUMIeERS9Dg&usqp=CAU'/>
-      Post 1
+      {props.message}
       <div>
-        <span>Like</span>
+        <span>Like {props.likesCount}</span>
       </div>
     </div>
   )
