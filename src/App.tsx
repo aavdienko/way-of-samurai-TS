25bc4b8e4bd05c7e77bacca9cef28f8ae0c80aa7
aavdienko <asiavostok@seznam.cz>
2023-01-09 19:55:11 +0100

    Lesson 19. Route. Browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import { Dialogs } from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 
-
 const App = () => {
   return (
-    <div className='app-wraper'>
-      <Header/>
-      <Navbar/>
-      <div className='app-wraper-content'>
-      {/* <Profile/> */}
-      <Dialogs/>
+    <BrowserRouter>
+      <div className="app-wraper">
+        <Header />
+        <Navbar />
+        <div className="app-wraper-content">
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={Profile} />
+          <Route path='/music' component={Profile} />
+          <Route path='/settings' component={Profile} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,10 +5,21 @@ import styles from './Navbar.module.css'
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <div className={`${styles.item} ${styles.active}`}><a>Profile</a></div>
-      <div className={styles.item}><a>Messages</a></div>
-      <div className={styles.item}><a>News</a></div>
-      <div className={styles.item}><a>Music</a></div>
+      <div className={`${styles.item} ${styles.active}`}>
+        <a href='/profile'>Profile</a>
+      </div>
+      <div className={styles.item}>
+        <a href='/dialogs'>Messages</a>
+      </div>
+      <div className={styles.item}>
+        <a href='/news'>News</a>
+      </div>
+      <div className={styles.item}>
+        <a href='/music'>Music</a>
+      </div>
+      <div className={styles.item}>
+        <a href='/settings'>Settings</a>
+      </div>
   </nav>
   )
 }
