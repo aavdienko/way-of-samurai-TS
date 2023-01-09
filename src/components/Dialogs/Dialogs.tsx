837fee105affc35ea879c830a813bf3c8c19a2f5
aavdienko <asiavostok@seznam.cz>
2023-01-09 20:40:43 +0100

    Lesson 22. Discussed Route Exact

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Dialogs.module.css'
 
 
 export const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog + ' ' + styles.active}>
+          <NavLink to='/dialogs/1'>Alex</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to='/dialogs/2'>Lera</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to='/dialogs/3'>Schoko</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to='/dialogs/4'>Crosby</NavLink>
+        </div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hi</div>
+        <div className={styles.message}>How are you?</div>
+        <div className={styles.message}>I'm Corsby</div>
+        <div className={styles.message}>Yo!</div>
+      </div>
     </div>
 )};
